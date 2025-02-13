7d39506684d8865109fa982abce5cd3125895eff
HuTaoMain28 <jgsbalane22@src.pshs.edu.ph>
2025-02-13 08:44:50 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function callFunc(){
+    let initialPop = parseFloat(prompt("Enter Initial Population: "));
+    let rateOfGrowth = parseFloat(prompt("Enter its rate of growth(number between 0 and 1): "));
+    let timeInHours = parseFloat(prompt("Enter time in hours: "));
+    let finalPop = Math.round(initialPop*Math.pow(Math.E, (rateOfGrowth*timeInHours)));
+    let locationOfMonster = prompt("Enter location of the monster: ");
+    let nameOfMonster = prompt("Enter the name of the monster: ");
+    let nameAndLocationOfMonster = locationOfMonster.concat(" ", nameOfMonster).toUpperCase();
+    document.getElementById("result").innerHTML = "  After  " + timeInHours + "  hours, the population of  " + nameAndLocationOfMonster + "  has risen to  " + finalPop;
+}
