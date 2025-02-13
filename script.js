46250fccecb0e934e6ee00a786898cc33a69dc21
HuTaoMain28 <jgsbalane22@src.pshs.edu.ph>
2025-02-13 08:51:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function callFunc(){
-    let initialPop = parseFloat(prompt("Enter Initial Population: "));
+    let initialPopulation = parseFloat(prompt("Enter Initial Population: "));
     let rateOfGrowth = parseFloat(prompt("Enter its rate of growth(number between 0 and 1): "));
     let timeInHours = parseFloat(prompt("Enter time in hours: "));
-    let finalPop = Math.round(initialPop*Math.pow(Math.E, (rateOfGrowth*timeInHours)));
+    let finalPop = Math.round(initialPopulation*Math.pow(Math.E, (rateOfGrowth*timeInHours)));
     let locationOfMonster = prompt("Enter location of the monster: ");
     let nameOfMonster = prompt("Enter the name of the monster: ");
     let nameAndLocationOfMonster = locationOfMonster.concat(" ", nameOfMonster).toUpperCase();
